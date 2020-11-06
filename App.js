298d4043a74f1c21dbb908889cfb24cc6ba1c4a4
Maxi <maxifiorito77@gmail.com>
2020-11-06 20:38:43 -0300

    Inicio-Bienvenida-Login

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/* Author: Maximiliano Fiorito, Fecha: 21/10/2020, Institucion: ISP*/
+import React from "react";
+import {
+  StyleSheet,
+  View,
+} from "react-native";
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} >
+      
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#6C0000',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+
+
---
+++ welcome.js
@@ -0,0 +1,60 @@
+/* Author: Maximiliano Fiorito, Fecha: 21/10/2020, Institucion: ISP*/
+import React from "react";
+import {
+    StyleSheet,
+    View,
+} from "react-native";
+import { Avatar, Button, Text } from "react-native-elements";
+
+class Welcome extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Avatar
+                    size="xlarge"
+                    rounded
+                    source={{
+                        uri:
+                            'https://cdn.discordapp.com/attachments/757361195425726585/773705725788553226/InstitutoLogo.png'
+                    }}
+                />
+                <Text h2 style={{ color: "white" }}>¡Bienvenido!</Text>
+                <Text h2 style={{ color: "white" }}>"Usuario"</Text>
+                <View style={stylesButton.container}>
+                    <Button
+                        title="Deslogearse"
+                        titleStyle={{ color: "white" }}
+                        buttonStyle={{ backgroundColor: "#6C0000" }}
+                    />
+                    <Button
+                        title="Continuar"
+                        titleStyle={{ color: "white" }}
+                        buttonStyle={{ backgroundColor: "#6C0000" }}
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#6C0000',
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
+
+const stylesButton = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#6C0000',
+        alignItems: "flex-end",
+        justifyContent: 'flex-end',
+        flexDirection: "row",
+    },
+});
+
+
+export default Welcome;
