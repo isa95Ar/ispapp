139e947f5953bf34a40ebb1998b2bd228d7cfffe
unknown <63322342+gonzaveron@users.noreply.github.com>
2020-11-11 21:28:05 -0300

    "casi bien formateado"

--- components/navigator/CustomDrawer.js
+++ components/navigator/CustomDrawer.js
@@ -42,16 +42,20 @@ export default function CustomDrawerContent(props) {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         width: "100%",
-        height: "40%",
+        height: 150,
+        alignItems: "flex-end"
     },
     nombreTitulo: {
+        flex: 1,
         flexDirection: "row", 
         justifyContent: "flex-end", 
         alignItems: "flex-end",
     },
     imagebckgrnd: {
         width: "100%",
+        height: 150,
         position: 'relative',
         top: -5,
         left: 0,
