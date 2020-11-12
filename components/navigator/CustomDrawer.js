29bbeadb090878e8343292e64a2ec36280945304
unknown <63322342+gonzaveron@users.noreply.github.com>
2020-11-11 21:50:36 -0300

    "commit final"

--- components/navigator/CustomDrawer.js
+++ components/navigator/CustomDrawer.js
@@ -7,47 +7,41 @@ import * as Linking from 'expo-linking';
 export default function CustomDrawerContent(props) {
     const image = require("../../assets/images/Papasito.png");
     const name = "Gonza Verón";
-    const title = "Alto papasito";
+    const title = "Pre-junior Developer";
     return (
         <DrawerContentScrollView {...props} style={styles.drawContent}>
-            <View style={styles.container}>
-                
-                <ImageBackground
-                        source={image}
-                        style={styles.imagebckgrnd}>
-                    <View style={styles.nombreTitulo}>
-                        <View style={{flexDirection: "column"}}>
-                            <Text style={{color: "#fff", fontWeight: "bold"}}>{name}</Text>
-                            <Text style={{color: "#fff", fontWeight: "bold"}}>{title}</Text>
-                        </View>
-                        <Avatar
-                            rounded
-                            size="large"
-                            source={{
-                                uri: image}} />
+            <ImageBackground
+                    source={image}
+                    imageStyle={{opacity:0.6}}
+                    style={styles.imagebckgrnd}>
+                <View style={styles.nombreTitulo}>
+                    <View style={{flexDirection: "column", marginBottom: "5%"}}>
+                        <Text style={{color: "#fff", fontWeight: "bold", opacity: 1}}>{name}</Text>
+                        <Text style={{color: "#fff", fontWeight: "bold", opacity: 1}}>{title}</Text>
                     </View>
-                </ImageBackground>
+                    <Avatar
+                        rounded
+                        size="large"
+                        source={{
+                            uri: image}} />
+                </View>
+            </ImageBackground>
+            <View style={{flex:1}}>
+                <DrawerItemList {...props} labelStyle={styles.drawItem} />
+                <DrawerItem
+                    label="Aiuda"
+                    labelStyle={styles.drawItem}
+                    onPress={() => Linking.openURL('https://www.google.com/search?q=programaci%C3%B3n+para+novatos')} />
+                <DrawerItem
+                    label="Cerrar Sesión"
+                    labelStyle={styles.drawItem}
+                    onPress={() => console.log("sesión cerrada")} />
             </View>
-            <DrawerItemList {...props} labelStyle={styles.drawItem} />
-            <DrawerItem
-                label="Aiuda"
-                labelStyle={styles.drawItem}
-                onPress={() => Linking.openURL('https://www.google.com/search?q=programaci%C3%B3n+para+novatos')} />
-            <DrawerItem
-                label="Cerrar Sesión"
-                labelStyle={styles.drawItem}
-                onPress={() => console.log("sesión cerrada")} />
         </DrawerContentScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: "100%",
-        height: 150,
-        alignItems: "flex-end"
-    },
     nombreTitulo: {
         flex: 1,
         flexDirection: "row", 
@@ -55,12 +49,12 @@ const styles = StyleSheet.create({
         alignItems: "flex-end",
     },
     imagebckgrnd: {
+        flex: 4,
         width: "100%",
-        height: 150,
+        height: "100%",
         position: 'relative',
         top: -5,
         left: 0,
-        opacity: 1,
     },
     drawContent: {
         backgroundColor: "brown",
