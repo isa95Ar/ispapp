5fc643266b79ac925df4dc240f8c54767e42357f
isaias <isaarg2312@gmail.com>
2020-11-13 23:03:53 -0300

    update screens

--- components/screens/login.js
+++ components/screens/login.js
@@ -1,47 +1,80 @@
 /* Author: Maximiliano Fiorito, Fecha: 21/10/2020, Institucion: ISP*/
-import React from "react";
+import React,{useRef} from "react";
 import { StyleSheet, View } from "react-native";
 import { Avatar, Input, Button } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function Login({ navigation }) {
+ 
+  const input = useRef(null);
+
   return (
     <View style={styles.container}>
-      <Avatar
-        size="xlarge"
-        rounded
-        source={{
-          uri:
-            "https://cdn.discordapp.com/attachments/757361195425726585/773705725788553226/InstitutoLogo.png",
-        }}
-      />
-      <Input
-        placeholder="Usuario"
-        leftIcon={<Icon name="user" size={24} color="black" />}
-      />
-      <Input
-        placeholder="Contraseña"
-        secureTextEntry={true}
-        leftIcon={<Icon name="lock" size={24} color="black" />}
-      />
-      <Button
-        title="Entrar"
-        titleStyle={{ color: "#6C0000" }}
-        buttonStyle={{ backgroundColor: "white" }}
-        onPress={() => {
-          navigation.navigate("Welcome");
-        }}
-      />
+      <View style={styles.containerHeader}>
+        <Avatar
+          size="xlarge"
+          rounded
+          source={{
+            uri:
+              "https://cdn.discordapp.com/attachments/757361195425726585/773705725788553226/InstitutoLogo.png",
+          }}
+        />
+      </View>
+      <View style={styles.containerInputs}>
+        <Input
+          
+          placeholder="Usuario"
+          placeholderTextColor="white"
+          leftIcon={<Icon name="user" size={24} color="white" />}
+        />
+        <Input
+          placeholder="Contraseña"
+          placeholderTextColor="white"
+          secureTextEntry={true}
+          leftIcon={<Icon name="lock" size={24} color="white" />}
+        />
+      </View>
+      <View style={styles.containerButtons}>
+        <Button
+          title="Entrar"
+          titleStyle={{ color: "#6C0000" }}
+          buttonStyle={{ backgroundColor: "white" }}
+          onPress={() => {
+            navigation.navigate("Welcome");
+          }}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  container : {
     backgroundColor: "#6C0000",
+    height: "100%",
+    widht: "100%",
+  },
+  containerHeader: {
+    flex: 2,
     alignItems: "center",
     justifyContent: "center",
+    height: "100%",
+    widht: "100%",
   },
+  containerInputs : {
+    flex:2,
+    flexDirection: 'column',
+    alignItems:"center",
+    justifyContent:"center",
+    height: "100%",
+    widht: "100%",
+  },
+  containerButtons : {
+    flex:2,
+    flexDirection: 'column',
+    alignItems:"center",
+    justifyContent:"center",
+    height: "100%",
+    widht: "100%",
+  }
 });
-
--- components/screens/welcome.js
+++ components/screens/welcome.js
@@ -3,24 +3,30 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import { Avatar, Button, Text } from "react-native-elements";
 
-export default function Welcome( {navigation} ) {
+export default function Welcome({ navigation }) {
   return (
     <View style={styles.container}>
-      <Avatar
-        size="xlarge"
-        rounded
-        source={{
-          uri:
-            "https://cdn.discordapp.com/attachments/757361195425726585/773705725788553226/InstitutoLogo.png",
-        }}
-      />
-      <Text h2 style={{ color: "white" }}>
-        ¡Bienvenido!
-      </Text>
-      <Text h2 style={{ color: "white" }}>
-        "Usuario"
-      </Text>
-      <View style={{ width: "70%", height: "100%", backgroundColor: "#6C0000", alignItems:"flex-start", flexDirection:"column-reverse", flex: 1}}>
+      <View style={styles.containerHeader}>
+        <Avatar
+          size="xlarge"
+          rounded
+          source={{
+            uri:
+              "https://cdn.discordapp.com/attachments/757361195425726585/773705725788553226/InstitutoLogo.png",
+          }}
+        />
+      </View>
+      <View style={styles.containerBody}>
+        <Text h1 style={{ color: "white",fontWeight: 'bold' }}>
+          ¡Bienvenido!
+        </Text>
+        <Text h2 style={{ color: "white" }}>
+          Maximiliano Fiorito
+        </Text>
+      </View>
+      <View
+        style={styles.containerFooter}
+      >
         <Button
           title="Deslogearse"
           titleStyle={{ color: "white" }}
@@ -29,8 +35,6 @@ export default function Welcome( {navigation} ) {
             navigation.navigate("Login");
           }}
         />
-        </View>
-        <View style={{ width: "70%", height: "80%", backgroundColor: "#6C0000", alignItems:"flex-end", flexDirection:"column", flex: 1 }}>
         <Button
           title="Continuar"
           titleStyle={{ color: "white" }}
@@ -46,14 +50,34 @@ export default function Welcome( {navigation} ) {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 3,
     width: "100%",
     height: "100%",
     backgroundColor: "#6C0000",
+  },
+  containerHeader: {
+    flex: 3,
+    width: "100%",
+    height: "100%",
+    flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
   },
+  containerBody: {
+    flex: 3,
+    width: "100%",
+    height: "100%",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  containerFooter : {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    flexDirection :"row",
+    alignContent : "center",
+    alignItems:"center",
+    justifyContent :"space-between",
+
+  }
 });
-
-
-
