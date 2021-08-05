d50da0eaf38507a6b671283b1e7120716bb95fdb
Isaias Diaz <isaias@animus.com.ar>
2021-08-05 16:56:35 -0300

    version 1,0,0

--- components/Post/index.js
+++ components/Post/index.js
@@ -8,14 +8,15 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import ModalCard from "./ModalCard";
 
 export default function Post() {
-  const [valor, setValor] = useState("Aviso");
+  const [valor, setValor] = useState("Consigna");
   const [list, setList] = useState([]);
   const [page, setPage] = useState(1);
-	const [modalVisible, setModalVisible] = useState(false);
-	const [modalData, setModalData] = useState({text: 'Texto texto descripción', title: 'Título', banner: {uri: ''}});
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalData, setModalData] = useState({ text: 'Texto texto descripción', title: 'Título', banner: { uri: '' } });
 
   const apiCall = async () => {
     try {
+      console.log(page, valor);
       const token = await AsyncStorage.getItem("session");
       const data = await fetch(`http://backend.institutopatagonico.edu.ar/api/posts?page=${page}&type=${valor}`, {
         method: 'GET',
@@ -26,17 +27,13 @@ export default function Post() {
         },
       });
       let response = await data.json();
-      
       setList(response);
     } catch (e) {
       console.log(e);
     }
   }
-  const Pages = () => {
-    if (page < 1) {
-      setPage(1)
-    }
-  }
+
+
 
   useEffect(() => {
     apiCall();
@@ -44,63 +41,72 @@ export default function Post() {
 
   useEffect(() => {
     apiCall();
-    setPage(1);
-  }, [valor]);
+  }, [valor, page]);
 
-  useEffect(() => {
-    apiCall();
-    Pages();
-  }, [page]);
+
 
   return (
 
-    <View>
-      <SafeAreaView style={styles.containerSafeArea}>
-				<ModalCard data={modalData} setVisible={setModalVisible} visible={modalVisible} />
-        <ScrollView>
-          <RNPickerSelect
-            items={[
-              { label: 'Consigna', value: 'Consigna' },
-              { label: 'Tarea', value: 'Tarea' },
-              { label: 'Aviso', value: 'Aviso' },
-            ]}
-            placeholder={{
-              label: "Elija un tipo de Post",
-              value: null,
-            }}
-            onValueChange={(value) => setValor(value)}
+    <View style={{
+      width: '100%',
+      height: '90%'
+    }}>
+
+      <ModalCard data={modalData} setVisible={setModalVisible} visible={modalVisible} />
+
+
+      <RNPickerSelect
+        items={[
+          { label: 'Consigna', value: 'Consigna' },
+          { label: 'Tarea', value: 'Tarea' },
+          { label: 'Aviso', value: 'Aviso' },
+        ]}
+        placeholder={{
+          label: 'Todos',
+          value: '',
+        }}
+        value={valor}
+        onValueChange={(value) => setValor(value)}
+      />
+
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+
+        {list.map((l, i) => (
+          <ListItem key={i} bottomDivider pad={30}>
+            <Avatar source={{ uri: l.banner }} size="small" style={styles.avatar} />
+            <ListItem.Content >
+              <ListItem.Title>{l.title}</ListItem.Title>
+              <ListItem.Subtitle style={styles.ratingText}>
+                <Text style={{ color: "red" }}>{l.type}</Text>
+                <Text> en </Text>
+                <Text style={{ color: "red" }}>{l.career_id}</Text>
+              </ListItem.Subtitle>
+            </ListItem.Content>
+            <ListItem.Chevron
+              onPress={() => {
+                setModalData(l);
+                setModalVisible(true);
+              }}
+            />
+          </ListItem>
+        ))}
+        <View style={styles.buttons}>
+          {list.length >= 25 && <><Button
+            onPress={() => setPage(page - 1)}
+            title="Anterior Página"
           />
-          {list.map((l, i) => (
-            <ListItem key={i} bottomDivider pad={30}>
-              <Avatar source={{ uri: l.banner }} size="small" style={styles.avatar} />
-              <ListItem.Content >
-                <ListItem.Title>{l.title}</ListItem.Title>
-                <ListItem.Subtitle style={styles.ratingText}>
-                  <Text style={{ color: "red" }}>{l.type}</Text>
-                  <Text> en </Text>
-                  <Text style={{ color: "red" }}>{l.career_id}</Text>
-                </ListItem.Subtitle>
-              </ListItem.Content>
-              <ListItem.Chevron 
-								onPress={() => {
-									setModalData(l);
-									setModalVisible(true);
-								}}
-							/>
-            </ListItem>
-          ))}
-          <View style={styles.buttons}>
-            <Button
-              onPress={() => setPage(page - 1)}
-              title="Anterior Página"
-            />
-            <Button
-              onPress={() => setPage(page + 1)}
-              title="Siguiente Página"
-            />
-          </View>
-          </ScrollView>
-      </SafeAreaView>
+          <Button
+            onPress={() => setPage(page + 1)}
+            title="Siguiente Página"
+          /></>}
+          
+        </View>
+      </ScrollView>
+
+
+
+
+
     </View>
   );
 }
@@ -118,7 +124,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-evenly",
     alignContent: "flex-end",
-    backgroundColor: "blue"
+    marginTop:20,
+    marginBottom:20
   },
   containerSafeArea: {
     backgroundColor: "red",
